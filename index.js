2e80f5f3ea5023d9aa1b1eed79322975c24b5f77
Sung Bae <singsang2@gmail.com>
2018-10-05 19:46:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ var greetSpecialGuest = "Hello, "+ specialGuest + "!";
 var topic = "space";
 
 var conversation = `Let's talk about ${topic}.`;
+
+
+var A = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+console.log(A.splice(0, 1))
